e52d4fad9707d3c3de08d79d1c7eb724094d6247
SamBritt <sam.britt2@yahoo.com>
2019-03-05 09:26:42 -0600

    march-05-lightning exercise

--- main.js
+++ main.js
@@ -1,60 +1,74 @@
-const students = [];
+// const students = [];
 
-const addStudent = (obj) => {
-    students.push(obj);
-}
+// const addStudent = (obj) => {
+//     students.push(obj);
+// }
 
-const student1 = {
-    name: "Sam",
-    subject: "Math",
-    score: 60
-}
-const student2 = {
-    name: "Katherine",
-    subject: "Game Design",
-    score: 20
-}
-const student3 = {
-    name: "Jacob",
-    subject: "Basket Weaving",
-    score: 45
-}
-const student4 = {
-    name: "Rose McGlue",
-    subject: "Crafts",
-    score: 30
-}
-const student5 = {
-    name: "Christopher",
-    subject: "English",
-    score: 80
-}
+// const student1 = {
+//     name: "Sam",
+//     subject: "Math",
+//     score: 60
+// }
+// const student2 = {
+//     name: "Katherine",
+//     subject: "Game Design",
+//     score: 20
+// }
+// const student3 = {
+//     name: "Jacob",
+//     subject: "Basket Weaving",
+//     score: 45
+// }
+// const student4 = {
+//     name: "Rose McGlue",
+//     subject: "Crafts",
+//     score: 30
+// }
+// const student5 = {
+//     name: "Christopher",
+//     subject: "English",
+//     score: 80
+// }
 
-addStudent(student1);
-addStudent(student2);
-addStudent(student3);
-addStudent(student4);
-addStudent(student5);
+// addStudent(student1);
+// addStudent(student2);
+// addStudent(student3);
+// addStudent(student4);
+// addStudent(student5);
 
-console.log(students);
+// console.log(students);
 
-for (let prop in students) {
-    console.log(`${prop.name} has a score of ${prop.score} in ${prop.subject}`);
-}
+// for (let prop in students) {
+//     console.log(`${prop.name} has a score of ${prop.score} in ${prop.subject}`);
+// }
 
-for (let i = 0; i < students.length; i++) {
-    if(students[i].score >= 70){
+// for (let i = 0; i < students.length; i++) {
+//     if(students[i].score >= 70){
 
-        console.log(`${students[i].name} has a score of ${students[i].score} in ${students[i].subject}`);
+//         console.log(`${students[i].name} has a score of ${students[i].score} in ${students[i].subject}`);
+//     }
+// }
+
+// let totalScore = 0;
+
+// for(let i = 0; i < students.length; i++){
+//     totalScore += students[i].score;
+// }
+// totalScore = totalScore / students.length;
+// console.log(totalScore);
+
+//Dog Breed
+const dogBreed = (breed) => {
+    if(breed){
+        return `My favorite breed of dog is: ${breed}`;
+    }else{
+       return `I like cats.`;
     }
 }
 
-let totalScore = 0;
-
-for(let i = 0; i < students.length; i++){
-    totalScore += students[i].score;
-}
-totalScore = totalScore / students.length;
-console.log(totalScore);
+const myFavBreed = dogBreed('Boxer');
+console.log(myFavBreed);
+console.log(`When it comes to pets, ${myFavBreed}`);
+console.log(dogBreed());
 
 
